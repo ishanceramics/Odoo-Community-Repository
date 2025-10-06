d32582ad2c0a7fdb3fb93aaae55ec9f7c7c76af1
ICPL <lokendra.ishanworld@gmail.com>
2025-10-06 15:33:24 +0530

    Create __init__.py

---
+++ odoo/custom addons/wk_franchisee_management/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+# Author : Webkul Software Pvt. Ltd. (<https://webkul.com/>:wink:
+# Copyright(c): 2015-Present Webkul Software Pvt. Ltd.
+# All Rights Reserved.
+#
+#
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#
+# You should have received a copy of the License along with this program.
+# If not, see <https://store.webkul.com/license.html/>;
+#################################################################################
+from . import franchisee_unit_template
+from . import res_company
+from . import product
+from . import manufacturer
+from . import product_family
+from . import product_slab
+from . import product_grade
+from . import product_series
+from . import product_brand
+from . import product_body_type
+from . import product_model
+from . import product_portfolio
+from . import product_category
+from . import product_size
+from . import item_group
+from . import purchase_order
+from . import purchase_requisition
+from . import wk_field_values
