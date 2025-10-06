4f64b9fbd5b060e99e2eb3b28bbfa3c68fb819ff
ICPL <lokendra.ishanworld@gmail.com>
2025-10-06 15:37:59 +0530

    Create static_franchisee_management_crm.py

---
+++ odoo/custom addons/wk_franchisee_crm/models/static_franchisee_management_crm.py
@@ -0,0 +1,40 @@
+WORKING_AREA = [
+        ('town', "Town"),
+        ('district', "District"),
+]
+COVERING_AREA = [
+        ('town', "Town"),
+        ('district', "District"),
+        ('district', "Assigned Territory"),
+        ('40_km', "40 Km appx"),
+]
+WAREHOUSE_LOCATION = [
+        ('store', "Adjoining to store"),
+        ('outskirt', "Outskirt of city or Industrial area"),
+]
+SEGMENT = [
+    ('b2b', "B2B"),
+    ('b2c', "B2C"),
+    ('both', "BOTH"),
+]
+TYPE = [
+    ('franchisee', "Franchisee"),
+]
+AGE = [
+        ('age_18_30', "18-30"),
+        ('age_31_45', "31-45"),
+        ('age_46_60', "46-60"),
+        ('age_61_75', "61-75"),
+]
+SHOWROOM_LOCATION = [
+        ('main_city', "Main City"),
+        ('main_city_or_highway', "Main City or Highway"),
+        ('adjoining_to_warehouse', "Adjoining to Warehouse"),
+        ('main_city_highway_outskirt_of_city', "Main City / Highway / outskirt of city"),
+]
+STATUS = [
+        ('under_construction', 'Under Construction'),
+        ('live', 'Live'),
+        ('hold', 'Hold'),
+        ('vacancy_live', 'Vacancy Live'),
+]
