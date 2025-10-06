37682059098fce2a5bee4fa9d56e3f7a51e61b64
ICPL <lokendra.ishanworld@gmail.com>
2025-10-06 15:38:28 +0530

    Create static_franchisee_management.py

---
+++ odoo/custom addons/wk_franchisee_management/models/static_franchisee_management.py
@@ -0,0 +1,89 @@
+UNIT_TYPE = [
+        ('coco', "COCO"),
+        ('foco', "FOCO"),
+  
+]
+
+FRANCHISEE_LEVEL = [
+    ('level_1', "Level 1"),
+    ('level_2', "Level 2")
+]
+
+
+ITEM_CLASSIFICATION = [
+    ('class_a', "A"),
+    ('class_b', "B"),
+    ('class_c', "C"),
+    ('class_d', "D")
+]
+
+
+PRODUCT_QUALITY = [
+    ('standard', "Standard"),
+    ('premium', "Premium"),
+]
+
+PRINTING_TYPE = [
+    ('standard', "Standard"),
+    ('digital', "Digital"),
+]
+
+SEGMENT = [
+    ('b2b', "B2B"),
+    ('b2c', "B2C"),
+    ('both', "BOTH"),
+]
+
+BRAND_TYPE = [
+    ('own', "Company Brand"),
+    ('other', "Other Brand"),
+]
+
+
+TRADE_TYPE = [
+    ('trade', "Trade"),
+    ('non_trade', "Non-Trade"),
+    ('both', "BOTH"),
+]
+
+INVENTORY = [
+    ('display', "Display"),
+    ('unit_warehouse', "Unit Warehouse"),
+]
+
+SHOWROOM_LOCATION = [
+    ('main_city', "Main City"),
+    ('main_city_or_highway', "Main City or Highway"),
+    ('adjoining_to_warehouse', "Adjoining to Warehouse"),
+    ('main_city_highway_outskirt_of_city', "Main City / Highway / outskirt of city"),
+]
+
+WAREHOUSE_LOCATION = [
+    ('store', "Adjoining to store"),
+    ('outskirt', "Outskirt of city or Industrial area"),
+]
+
+WORKING_AREA = [
+    ('town', "Town"),
+    ('district', "District"),
+]
+COVERING_AREA = [
+    ('town', "Town"),
+    ('district', "District"),
+    ('district', "Assigned Territory"),
+    ('40_km', "40 Km appx"),
+]
+
+FIELD_TYPE = [
+    ('age', "Age"),
+    ('firm_type', "Firm Type"),
+    ('experience_in_business', "Experience In Business(Yr)"),
+    ('current_profession', "Your Current Profession"),
+    ('property_status', "Your Property Status"),
+    ('looking_for_franchisee', "Looking For This Franchisee For"),
+    ('is_this_property_available', "Is This Property Available"),
+    ('working_area', "Working Area"),
+    ('covering_area', "Covering Area"),
+    ('warehouse_location', "Warehouse Location"),
+    ('showroom_location', "Showroom Location"),
+]
